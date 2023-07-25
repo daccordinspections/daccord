9465a288228c165ebab831b514a3fb18bfc7174d
unknown <cameron@floids.co>
2023-07-25 06:54:00 +0530

    removed unnessesary code utils/mongodb.js

--- server/runServer.js
+++ server/runServer.js
@@ -6,7 +6,11 @@ module.exports = (app) => {
     const PORT = process.env.PORT;
 
     app.listen(PORT, () => {
-        console.log(`Bonjour! the D'accord server is running on http://localhost:${PORT}`);
+        if (process.env.NODE_ENV !== "production") {
+            console.log(`Bonjour! the D'accord server is running on http://localhost:${PORT}`);
+        } else {
+            console.log(`Bonjour! the D'accord server is running...`);
+        }
     });
 
 }
